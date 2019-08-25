bc9940831e926ada4de8c39d9b5d7c6951f4065b
zz <32082888+zainz23@users.noreply.github.com>
2019-08-24 20:17:07 -0500

    Adds 2nd solution to 2-6

--- 2. Linked Lists/2-6.java
+++ 2. Linked Lists/2-6.java
@@ -1,3 +1,5 @@
+// Solution 1
+/*
 public boolean isPalindrome(ListNode head) {
     Stack<ListNode> s = new Stack<ListNode>();
     ListNode p = head;
@@ -11,6 +13,37 @@ public boolean isPalindrome(ListNode head) {
             return false;
         }
         p = p.next;
+    }
+    return true;
+}
+*/
+
+// Solution 2 (preferred) - Compare 1st half w/ 2nd half of list
+public boolean isPalindrome(ListNode head) {
+    // Solution 2
+    ListNode slow = head;   // Iterates at default speed
+    ListNode fast = head;   // Iterates 2x as fast
+
+    Stack<ListNode> s = new Stack<ListNode>();  // Stack for 1st half
+
+    while (fast != null && fast.next != null) {
+        s.push(slow);           // Push to first half stack
+        slow = slow.next;       // 1x
+        fast = fast.next.next;  // 2x
+    }
+
+    // When fast is == null, we know were at middle for slow
+    // Odd number, so we go further
+    if (fast != null) {
+        slow = slow.next;   // skip that 1 middle element (ie. "131" 3 is not needed)
+    }
+
+    // Compare starting from slow's mid position to stack
+    while (slow != null) {
+        if (slow.val != (s.pop()).val) {
+            return false;
+        }
+        slow = slow.next;   // traverse further in 2nd half
     }
     return true;
 }
